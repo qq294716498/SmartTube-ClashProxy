58c8e467e8006095df34df966293133ae21a9300
qq294716498 <47776530+qq294716498@users.noreply.github.com>
2026-09-19 11:59:41 -0400

    feat: add subscription and node data layer

---
+++ smarttubetv/src/stproxy/java/com/liskovsoft/smartyoutubetv2/tv/proxy/model/ProxyNode.java
@@ -0,0 +1,21 @@
+package com.liskovsoft.smartyoutubetv2.tv.proxy.model;
+
+public final class ProxyNode {
+    public static final int DELAY_UNKNOWN = 0;
+    public static final int DELAY_TESTING = -2;
+    public static final int DELAY_TIMEOUT = -1;
+
+    public final String name;
+    public final String runtimeName;
+    public final String type;
+    public final boolean automatic;
+    public boolean selected;
+    public int delayMs;
+
+    public ProxyNode(String name, String runtimeName, String type, boolean automatic) {
+        this.name = name;
+        this.runtimeName = runtimeName;
+        this.type = type;
+        this.automatic = automatic;
+    }
+}
